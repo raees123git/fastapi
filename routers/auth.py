3ca0838e2eb14c812cc3109521d2efc350d43050
raees123git <zakirraees91@gmail.com>
2025-02-09 03:27:21 +0500

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -3,8 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status, HTTPException, Request
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
-from ..models import Users
+from database import SessionLocal
+from models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
