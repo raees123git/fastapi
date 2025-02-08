4fcade86fb29f8c81d2b4afb0c4ef49292798ac4
raees123git <zakirraees91@gmail.com>
2025-02-09 03:27:45 +0500

    Update todos.py

--- routers/todos.py
+++ routers/todos.py
@@ -2,9 +2,9 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Path, status, Request
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from ..models import Todos, Users
-from ..database import SessionLocal
-from .auth import get_current_user
+from models import Todos, Users
+from database import SessionLocal
+from auth import get_current_user
 from starlette.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
